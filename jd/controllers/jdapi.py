711418dff09c8e0398904c249e6428ab45c50d33
jof2jc <jof2jc@gmail.com>
2022-07-25 14:36:38 +0000

    add more validation update logic for refresh_token and new_auth_code

--- jd/controllers/jdapi.py
+++ jd/controllers/jdapi.py
@@ -4,6 +4,7 @@ from jd.api.base import RestApi
 import frappe, json
 from jd.controllers.jdauth import AuthClient, AuthRequest
 from frappe.utils import logger, cstr, cint, convert_utc_to_user_timezone, now, flt, time_diff_in_hours, now_datetime, nowdate, getdate, get_weekdays, add_days, add_to_date, today, get_time, get_datetime
+import ast
 
 class JdRequest(RestApi):
 	def __init__(self, api , endpoint):
@@ -25,7 +26,6 @@ class JdRequest(RestApi):
 	def get_response(self, access_token=None):
 		return self.getResponse(access_token=access_token,ssl=self.ssl)
 
-
 class JdClient(object):
 	def __init__(self, key, secret):
 		self.app_key = key
@@ -37,10 +37,13 @@ class JdClient(object):
 
 	def execute(self,request, access_token=None):
 		try:
-			print("access_token:", access_token)
-			print("JdClient execute >>>", request.__dict__)
-			response = request.get_response(access_token=access_token)
-			print(cstr(response))
+			#print("access_token:", access_token)
+			#print("JdClient execute >>>", request.__dict__)
+			response = request.get_response(access_token=access_token) or {}
+			print("JDClient Response", cstr(response))
+			if cint(response.get("code"))!=0:
+				frappe.log_error(cstr(request.__dict__) + "\n\n" + cstr(response),title="Error JDClient execute")
+
 			return response
 
 		except Exception as e:
@@ -49,13 +52,12 @@ class JdClient(object):
 				frappe.log_error(cstr(request.__dict__) + "\n\n" + error_trace,title="Error JdClient execute")
 
 
-def get_access_token_jd(api_doc=None,headers=None,body="", commit_flag=False):
+def get_access_token_jd(api_doc,headers=None,body="", commit_flag=False, new_auth_code=""):
 	access_token = ""
 	for t in api_doc.get("api_end_point",{"disabled":0,"type":("in",["Refresh Token","Access Token"])}):
-		if api_doc.marketplace_type == 'JDID' and t.type == "Refresh Token":
+		if api_doc.marketplace_type == 'JDID' and t.type == "Refresh Token" and not new_auth_code:
 			body={}
 			hour_factor=0
-			import ast
 			access_token = t.access_token or "" #use previous token first
 
 			if t.body:
@@ -77,6 +79,27 @@ def get_access_token_jd(api_doc=None,headers=None,body="", commit_flag=False):
 						t.db_set("access_token", access_token)
 						t.db_set("refresh_token", cstr(response.get("refresh_token")) or "")		
 						if commit_flag: frappe.db.commit()	
+		
+		elif api_doc.marketplace_type == 'JDID' and t.type == "Access Token" and new_auth_code: #re-generate refresh token based on new authorization code
+			t.authorization_code = new_auth_code
+			response = get_jd_response(t, api_doc) or {}
+			if response:
+				if response.get("access_token"):
+					print("new access_token_jd: ", response.get("access_token"))
+					access_token = cstr(response.get("access_token")) or ""
+
+				for r in api_doc.get("api_end_point",{"disabled":0,"type":("in",["Refresh Token"])}):
+					if r.type == "Refresh Token" and access_token:
+						r.db_set("authorization_code",new_auth_code)
+						r.db_set("last_token_created",now_datetime())
+						r.db_set("body",cstr(response))
+						r.db_set("order_detail_json","")
+						r.db_set("access_token", access_token)
+						r.db_set("refresh_token", cstr(response.get("refresh_token")) or "")
+
+						frappe.db.commit()
+
+				return response
 
 	return access_token or ""
 
@@ -86,8 +109,16 @@ def get_jd_response(t, api_doc, access_token='', d=None, item=None):
 	request, response, created_before = None, None, None
 
 	headers = json.loads(r.header.strip())
-	client = eval(headers.get("client"))
-	request = eval(headers.get("request"))	
+	client = None
+	request = None
+	try:
+		client = eval(headers.get("client"))
+		request = eval(headers.get("request"))	
+	except Exception as e:
+		error_trace = frappe.get_traceback()
+		if error_trace: 
+			frappe.log_error(cstr(headers) + "\n\n" + error_trace,title="%s, %s: Error JD Headers" % (api_doc.name, t.type))
+
 
 	for k,v in headers.items():
 	 	if "param" in k: eval(v)
--- jd/controllers/jdauth.py
+++ jd/controllers/jdauth.py
@@ -2,14 +2,17 @@
 from urllib.parse import urlparse
 import requests
 import frappe
-from frappe.utils import cstr
+from frappe.utils import cstr, cint
 
 class AuthRequest(object):
 	def __init__(self, refresh_token=None , auth_code=None, endpoint=None):
 		self.refresh_token = refresh_token
 		self.auth_code = auth_code
 		self.endpoint = endpoint
-		self.grant_type = "refresh_token" if refresh_token else "authorization_code"
+		if refresh_token:
+			self.grant_type = "refresh_token" 
+		elif auth_code: 
+			self.grant_type = "authorization_code"
 
 class AuthClient(object):
 	def __init__(self, app_key, app_secret):
@@ -20,18 +23,21 @@ class AuthClient(object):
 		url = None
 		if request.grant_type == "refresh_token":
 			url = f'{request.endpoint}?app_key={self.app_key}&app_secret={self.app_secret}&grant_type=refresh_token&refresh_token={request.refresh_token}'
-		else:
+		elif request.grant_type == "authorization_code":
 			url = f'{request.endpoint}?app_key={self.app_key}&app_secret={self.app_secret}&grant_type=authorization_code&code={request.auth_code}'
 
 		if url:
 			try:
-				print(url)
-				response = requests.get(url=url)
+				#print(url)
+				response = requests.get(url=url) or {}
 				print(response.json())
-				return response.json()
+				response = response.json()
+				if cint(response.get("code")):
+					frappe.log_error(cstr(request.__dict__) + "\n\n" + cstr(url) + "\n\n" + cstr(response),title="Error JDAuth execute: %s" % request.grant_type)
+				return response
 			except Exception as e:
 				error_trace = frappe.get_traceback()
 				if error_trace:
 					print(error_trace)
-					frappe.log_error(cstr(request.__dict__) + "\n\n" + error_trace,title="Error JD auth execute: %s" % request.grant_type)
+					frappe.log_error(cstr(request.__dict__) + "\n\n" + error_trace,title="Error JDAuth execute: %s" % request.grant_type)
 
